bdb80f316f60250861670521681cae5862888291
Rob Harkiss <rob.harkiss@audatex.co.uk>
2017-08-31 09:13:21 +0100

    commiting sample

---
+++ DotNetCoreApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using DotNetStandard.Whois;
+using System;
+using System.ServiceModel;
+
+namespace DotNetCoreApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var binder = new BasicHttpBinding();
+            binder.UseDefaultWebProxy = false;
+            binder.ProxyAddress = new Uri("http://DEFAULTPROXY:8080");
+
+            var remoteAddress = new EndpointAddress("http://www.webservicex.net/whois.asmx");
+            var client = new whoisSoapClient(binder, remoteAddress);
+
+            var response = client.GetWhoIS("microsoft.com");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # BasicHttpBindingSample
+
+Sample for issue https://github.com/dotnet/corefx/issues/23646
